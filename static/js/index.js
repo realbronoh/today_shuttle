f89b5d7bb78ff8e23e58d7fd6ac1ff6ddd9aa271
dapsu <greenrock46@gmail.com>
2021-08-03 20:39:33 +0900

    JS 및 템플릿 수정 (템플릿은 진형쓰껄로)

--- static/js/index.js
+++ static/js/index.js
@@ -1,50 +1,80 @@
-$(document).ready(function () {
-    // $('.memo-wrap').empty();
-    // $("#last-cards").empty();
-    showCards();
-  });
+// $(document).ready(function () {
+//     // $('.memo-wrap').empty();
+//     // $("#last-cards").empty();
+//     showCards();
+//   });
 
 
-function showCards() {
-    $.ajax({
-        type: "GET",
-        url: "/allshuttles",
-        data: {},
-        success: function (response) {
-            let cards = response["allshuttle"];
-            for (let i = 0; i < cards.length; i++) {
-                for (let j = 0; j < cards[i]['content'].length; j++) {
-                    makeCard(cards[i]['data'], cards[i]['winner'], i, j)
-                }
-            }
-        }
-    })
-}
+// function showCards() {
+//     $.ajax({
+//         type: "GET",
+//         url: "/allshuttles",
+//         data: {},
+//         success: function (response) {
+//             let cards = response["allshuttle"];
+//             for (let i = 0; i < cards.length; i++) {
+//                 for (let j = 0; j < cards[i]['content'].length; j++) {
+//                     makeCard(cards[i]['data'], cards[i]['winner'], i, j)
+//                 }
+//             }
+//         }
+//     })
+// }
 
-function makeCard(days, winners, i, j) {  // 진자템플릿 라우팅 필요
-    let temp_html = `
-    <div class="wrap">
-      <div class="p-5 mb-4 bg-light rounded-3">
-        <div class="container-fluid py-5">
-            <h1 class="display-5 fw-bold">${days}의 셔틀 : ${winners}</h1>
-            <div class="input-box">
-            </div>
-            <div class="memo-wrap">
-            {% for shuttle in shuttles %}
-                <div class="memo-list">
-                    <span class="memo-name">{{shuttle[${i}]['content'][${j}]['name']}}</span>
-                    <span class="memo-item">{{shuttle[${i}]['content'][${j}]['item']}}</span>
-            {% endfor %} 
-            </div>
-        </div>
-      </div>
-    </div>
-    `;
-    $("#last-cards").append(temp_html);
-}
+// function makeCard(days, winners, i, j) {  // 진자템플릿 라우팅 필요
+//     let temp_html = `
+//     <div class="wrap">
+//       <div class="p-5 mb-4 bg-light rounded-3">
+//         <div class="container-fluid py-5">
+//             <h1 class="display-5 fw-bold">${days}의 셔틀 : ${winners}</h1>
+//             <div class="input-box">
+//             </div>
+//             <div class="memo-wrap">
+//             {% for shuttle in shuttles %}
+//                 <div class="memo-list">
+//                     <span class="memo-name">{{shuttle[${i}]['content'][${j}]['name']}}</span>
+//                     <span class="memo-item">{{shuttle[${i}]['content'][${j}]['item']}}</span>
+//             {% endfor %} 
+//             </div>
+//         </div>
+//       </div>
+//     </div>
+//     `;
+//     $("#last-cards").append(temp_html);
+// }
+
+// function showMemos() {
+//     $.ajax({
+    //         type: "GET",
+    //         url: "",
+    //         data: {},
+//         success: function(response) {
+    //             let item_memos = response['memos'];  // 메모창 받아오기
+//             for (let i = 0; i < item_memos.length; i++) {
+    //                 for (let j = 0; j < item_memos[i]['content'].length; j++)
+    //                 makeMemo(item_memos[i]['content'][j]['name'], item_memos[i]['content'][j]['item'], j)
+//             }
+//         }
+//     })
+// }
+
+// function makeMemo(name, item, j) {
+//     let temp_memos = `
+//     <div class="memo-list">
+//         <span class="memo-name">${name}</span>
+//         <span class="memo-item">${item}</span>
+//         <span class="memo-del${j}"><button class="btn btn-dark btn-sm">삭제</button></span>
+//     </div>
+//     `;
+//     $('.memo-wrap').append(temp_memos);
+//     $(`.memo-del${j}`).click(function() {
+    //         deleteBtn(name);
+//     })
+// }
 
 function postShuttle() {
     let items = $('input-item').val();
+    
 
     $.ajax({
         type: "POST",
@@ -56,35 +86,6 @@ function postShuttle() {
                 window.location.reload();
             }
         }
-    })
-}
-
-function showMemos() {
-    $.ajax({
-        type: "GET",
-        url: "",
-        data: {},
-        success: function(response) {
-            let item_memos = response['memos'];  // 메모창 받아오기
-            for (let i = 0; i < item_memos.length; i++) {
-                for (let j = 0; j < item_memos[i]['content'].length; j++)
-                makeMemo(item_memos[i]['content'][j]['name'], item_memos[i]['content'][j]['item'], j)
-            }
-        }
-    })
-}
-
-function makeMemo(name, item, j) {
-    let temp_memos = `
-    <div class="memo-list">
-        <span class="memo-name">${name}</span>
-        <span class="memo-item">${item}</span>
-        <span class="memo-del${j}"><button class="btn btn-dark btn-sm">삭제</button></span>
-    </div>
-    `;
-    $('.memo-wrap').append(temp_memos);
-    $(`.memo-del${j}`).click(function() {
-        deleteBtn(name);
     })
 }
 
