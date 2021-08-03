a9294807a9b2acdc0706c67470ad5c6ca534a909
jungbum <jungbumwoo@naver.com>
2021-08-03 15:27:36 +0900

    trying to multithread

--- app.py
+++ app.py
@@ -1,43 +1,37 @@
 from flask import Flask
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('3.35.205.48', 27017)
+db = client.today_shuttle
+
 from apscheduler.schedulers.blocking import BlockingScheduler
  # 실행할 함수
 
-def exec_cron():
-    print('exec cron 크론이 실행되었습니다.')
+# import threading
+import multiprocessing
 
-sched = BlockingScheduler()
+def cronwork():
+    def exec_cron():
+        print('1분 마다 exec cron 크론이 실행되었습니다.')
 
-# 예약방식 interval로 설정, 10초마다 한번 실행
-# sched.add_job(exec_cron, 'interval', seconds=10)
+    def exec_cron2():
+        print('지정된 시간에 exec cron 크론이 실행되었습니다.')
 
-# 예약방식 cron으로 설정, 각 5배수 분의 10, 30초마다 실행
-# ex) (5분 10, 30초), (10분 10, 30초), (15분 10, 30초)
-# sched.add_job(exec_cron, 'cron', minute='*/5', second='10, 30')
+    sched = BlockingScheduler()
 
-sched.add_job(exec_cron, 'cron', minute='*/5', second='10')
+    # 예약방식 interval로 설정, 10초마다 한번 실행
+    # sched.add_job(exec_cron, 'interval', seconds=10)
 
-# 스케줄링 시작
-sched.start()
+    # 예약방식 cron으로 설정, 각 5배수 분의 10, 30초마다 실행
+    # ex) (5분 10, 30초), (10분 10, 30초), (15분 10, 30초)
+    sched.add_job(exec_cron, 'cron', minute='*/5', second='10, 30')
 
-# import time
-# import schedule
+    sched.add_job(exec_cron, 'cron', minute='*/1', second='10')
 
-# def printhello():
-#     print("Hello")
+    sched.add_job(exec_cron2, 'cron', hour='14', minute='25', second='10')
 
-# def printlunch():
-#     print("점심시간입니다")
-
-# schedule.every().day.at("13:10").do(printlunch) # 매일 12시에 실행
-# schedule.every(2).minutes.do(printhello) # 3분마다 실행
-
-# #스케줄 실행
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1) 
-
+    sched.start() # 스케줄링 시작
 
 
 @app.route('/')
@@ -47,6 +41,7 @@ def home():
 # main data 불러오기
 @app.route('/allshuttles', methods=['GET'])
 def allshuttles():
+    allshuttle = db.shuttles.find({}, {'_id': 0})
     return;
 
 # item 추가
@@ -59,5 +54,9 @@ def post_additem():
     return
 
 if __name__ == '__main__':  
-   app.run('0.0.0.0', port=5000, debug=True)
-
+    t = multiprocessing.Process(target=cronwork)
+    t.start()
+    t.join()
+    print("무야호")
+    app.run('0.0.0.0', port=5000, debug=True, threaded=True)
+   
