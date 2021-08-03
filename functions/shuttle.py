5e2e31d2bc81f3485c72bb7b78a394d06ec01417
jinh <realbro.noh@gmail.com>
2021-08-03 22:06:58 +0900

    jinh - main 수정

---
+++ functions/shuttle.py
@@ -0,0 +1,49 @@
+
+from flask import request, session, jsonify
+from pymongo import MongoClient
+import datetime
+
+##########################
+# DB
+client = MongoClient('3.35.205.48', 27017)
+db = client.today_shuttle
+
+class Shuttle():
+
+    def new_today_shuttle(self):
+        now = datetime.datetime.now()
+        nowDate = now.strftime('%Y-%m-%d')
+
+        today_shuttle = {
+            'date': nowDate,
+            'content': []
+        }
+
+        db.shuttles.insert_one(today_shuttle)
+        return None
+
+    def post_additem(self):
+        item = request.form['items_give']
+        
+        userID = session['user']['userID']
+        print(userID) #
+
+        now = datetime.datetime.now()
+        nowDate = now.strftime('%Y-%m-%d')
+
+        new_items = {
+            'user': userID,
+            'items': item
+        }
+
+        additem_result = db.items.insert_one(new_items)
+        print(new_items)
+
+        if db.shuttles.update_one({'date': nowDate}, { '$push' : { 'content': new_items }}):
+            return jsonify({'result': 'success'}), 200
+        return jsonify({"error": "post item failed"}), 400
+
+        # db.shuttles.update_one({'date': nowDate}, { '$push' : { 'content': new_items }})
+        # db.shuttles.update_one({'data': nowDate}, new_items)
+
+
